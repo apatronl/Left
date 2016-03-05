05b9097cc4c9a67057a1eea67d4d30718b3306ab
apatronl <ale7feb@hotmail.com>
2016-03-04 20:53:25 -0500

    Fav

--- Left/RecipeItem.swift
+++ Left/RecipeItem.swift
@@ -14,6 +14,24 @@ class RecipeItem: NSObject {
     var photo: UIImage?
     var url: String
     
+    let nameKey = "name"
+    let photoKey = "photo"
+    let urlKey = "url"
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: nameKey)
+        if let thePhoto = photo {
+            aCoder.encodeObject(thePhoto, forKey: photoKey)
+        }
+        aCoder.encodeObject(url, forKey: urlKey)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObjectForKey(nameKey) as! String
+        photo = aDecoder.decodeObjectForKey(photoKey) as? UIImage
+        url = aDecoder.decodeObjectForKey(urlKey) as! String
+    }
+    
     init(name: String, photo: UIImage?, url: String) {
         self.name = name
         self.photo = photo
