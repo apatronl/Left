d776b1b5658c741f65d0bd2028608e9aad5ec014
apatronl <ale7feb@hotmail.com>
2017-05-19 23:27:59 +0200

    Functionality to ask for user reviews

--- Left/RecipeItem.swift
+++ Left/RecipeItem.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2015 Ale Patrón. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 let RecipeOpenCountKey = "recipeOpenCount"
---
+++ Left/SKStoreReviewManager.swift
@@ -0,0 +1,36 @@
+//
+//  SKStoreReviewManager.swift
+//  Left
+//
+//  Created by Alejandrina Patron on 5/14/17.
+//  Copyright © 2017 Ale Patrón. All rights reserved.
+//
+
+import StoreKit
+
+struct SKStoreReviewManager {
+    
+    private static let APP_RUNS_KEY = "APP_RUNS"
+    
+    // Call this function in AppDelegate.swift on app launch
+    static func incrementAppRuns() {
+        let defaults = UserDefaults.standard
+        var appRuns = defaults.value(forKey: APP_RUNS_KEY) as? Int ?? 0
+        appRuns += 1
+        defaults.set(appRuns, forKey: APP_RUNS_KEY)
+    }
+    
+    static func askForReview() {
+        let defaults = UserDefaults.standard
+        let appRuns = defaults.value(forKey: APP_RUNS_KEY) as? Int ?? 0
+        
+        if #available(iOS 10.3, *) {
+            if appRuns > 0 && appRuns % 15 == 0 {
+                SKStoreReviewController.requestReview()
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+}
